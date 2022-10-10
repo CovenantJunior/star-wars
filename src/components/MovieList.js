a64fe09829d55551d241a83498943c11bb4979c0
Covenant Junior <covenant@gmail.com>
2022-10-10 23:08:35 +0100

    Tap Here

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,7 +1,7 @@
 const MovieList = ({movies, Change}) => {
     return (
         <select  id="film" name="film" onChange={Change} className="animate-box pulse animated-fast" style={{animationIterationCount: "infinite"}}>
-            <option></option>
+            <option>Tap Here</option>
             {movies.map((movie) => (
                 <option key={movie.episode_id} value={movie.episode_id} id={movie.episode_id}>{movie.title}</option>
             )).reverse()}
